c9813452a09ccee6cc99739ce5a7ec3710517441
HayatoInoue-git <57336594+HayatoInoue-git@users.noreply.github.com>
2024-11-19 10:57:18 +0000

    Initial Commit

---
+++ MainApp/MainAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MainAppApp.swift
+//  MainApp
+//
+//  Created by HayatoInoue on 2024/11/19.
+//
+
+import SwiftUI
+
+@main
+struct MainAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
